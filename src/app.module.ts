c13114b272d930b52eb98e4f7ac614a9c22681ad
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-07-01 23:57:42 -0300

    feat: dockerize the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { LineModule } from './line/line.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, LineModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
